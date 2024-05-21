692b201242118d8920e146e3037e73b428dcb190
Harizone lopez <harizonelopez23@gmail.com>
2024-05-21 12:34:51 +0300

    created main app

---
+++ myblog/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Post
+from .forms import PostForm
+from django.contrib import messages
+
+
+def home(request):
+    posts = Post.objects.all()
+    return render(request, 'home.html', {'posts': posts})
+
+def new_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            messages.success(request, 'Post created successfully!')
+            return redirect('home')
+    else:
+        form = PostForm()
+    return render(request, 'post_new.html', {'form': form})
+
+def edit_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            messages.success(request, 'Post updated successfully!')
+            return redirect('home')
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'post_new.html', {'form': form})
+
+def delete_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    
+    if request.method == "POST":
+        post.delete()
+        messages.success(request, 'Post deleted successfully!')
+        return redirect('home')  
+
+
+
+
+
+        
---
+++ myblog_project/urls.py
@@ -0,0 +1,33 @@
+"""
+URL configuration for myblog_project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from myblog import views as myblog_views 
+from myblog.views import home, delete_post
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home),
+    path('myblog/', include('myblog.urls')),
+
+    # user urls
+    path('home/', myblog_views.home, name='home'), 
+    path('new/', myblog_views.new_post, name='new_post'),
+    path('edit/<int:pk>/', myblog_views.edit_post, name='edit_post'),
+    path('post/<int:pk>/delete/', myblog_views.delete_post, name='delete_post'),
+]
+
