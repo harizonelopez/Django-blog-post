473154bb264dd6259bd05ae93ef81a179ba96a9f
Harizone lopez <136503275+harizonelopez@users.noreply.github.com>
2024-05-22 11:24:42 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,7 +2,6 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myblog_project.settings')
@@ -15,7 +14,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
